f29f6dfbc44316becf4291301451366efd1d95c7
emamoldo <moldovanemanuel92.em@gmail.com>
2024-05-29 09:01:33 +0200

    Initial commit:
    Add the auth installation and connect the db

---
+++ app/Models/Project.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Project extends Model
+{
+    use HasFactory;
+    protected $fillable = ['title', 'slug', 'content', 'cover_image', 'category_id', 'user_id'];
+
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function technologys(): BelongsToMany
+    {
+        return $this->belongsToMany(Technology::class);
+    }
+}
---
+++ app/Models/Technology.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Technology extends Model
+{
+    use HasFactory;
+
+    public function technologys(): BelongsToMany
+    {
+        return $this->belongsToMany(Technology::class);
+    }
+}
